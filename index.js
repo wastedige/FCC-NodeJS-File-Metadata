ca157fa56b435b945dc8193c5895f82f9c7f72eb
Shaahin <shaahin@gmail.com>
2016-05-01 13:30:25 -0500

    Form page receives data back, shows it as popup

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var multer  = require('multer')
+var upload = multer({ dest: 'uploads/' })
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -13,8 +15,14 @@ app.get('/', function(request, response) {
   response.render('pages/index');
 });
 
+app.post('/api/fileanalyse', upload.single('the-file'),  function(req, res, next) {
+  console.log({fileSize: JSON.stringify(req.file.size)} )
+//  res.status(200).send({success: true}, {fileSize: JSON.stringify(req.file.size)} )
+  //res.json({success: true});
+  res.end( JSON.stringify ( { fileSize: req.file.size } ) )
+
+})
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
-
-
